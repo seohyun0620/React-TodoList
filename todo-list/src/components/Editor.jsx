6f2ea5244717fd0068a95d59841496fb5f946f42
seohyun0620 <tjgus7924@naver.com>
2024-07-31 01:39:20 +0900

    [style] 전체적인 UI 수정

--- todo-list/src/components/Editor.jsx
+++ todo-list/src/components/Editor.jsx
@@ -3,7 +3,7 @@ import styles from './Editor.module.css';
 function Editor(){
   return(
     <>
-    <label className={styles.label}>
+    <label className={styles.Editorlabel}>
       <input className={styles.input} type="text" placeholder="새로운 Todo를 입력..." value=""/>
       <input className={styles.submitBtn} type="submit" value="추가" />
     </label>
