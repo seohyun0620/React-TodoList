98b26ce25fb2da9cf4da6834d764675f9bf0e0de
seohyun0620 <tjgus7924@naver.com>
2024-08-30 00:23:34 +0900

    [comment] 추가 기능(Create) 주석 추가

--- todo-list/src/components/Editor.jsx
+++ todo-list/src/components/Editor.jsx
@@ -24,7 +24,8 @@ function Editor(){
     <h3>Todo List📌</h3>
     <input className={styles.searchInput} placeholder="검색어를 입력하세요.." />
     {toDoList.map(listItem=>(<List key={listItem.text} todo={listItem.text}/>))}
-    {/*26 : todo목록 배열의 항목들을 하나씩 순회하며 각 항목에 있는 text의 값을 List컴포넌트의 todo props로 전달하며 렌더링 됨*/}
+    {/*26 : todo목록 배열의 항목들을 하나씩 순회하며 각 항목에 있는 text의 값을 List컴포넌트의 todo props로 전달하며 새 배열을 렌더링 함
+    이 새 배열은 JSX에서 사용 가능한 리액트 엘리먼트들로 구성된 배열이 됨*/}
     </>
   )
 }
